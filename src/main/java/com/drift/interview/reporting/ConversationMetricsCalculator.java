b44604a2fb9d7225d88a06ba3986b30e1cb05326
Ahmad Alhayek <a_alhayek@salemstate.edu>
2021-04-08 09:05:43 -0400

    Implemented ConversationMetricsCalculator

--- src/main/java/com/drift/interview/reporting/ConversationMetricsCalculator.java
+++ src/main/java/com/drift/interview/reporting/ConversationMetricsCalculator.java
@@ -14,11 +14,54 @@ public class ConversationMetricsCalculator {
   ConversationResponseMetric calculateAverageResponseTime(Conversation conversation) {
     List<Message> messages = conversation.getMessages();
 
+
+    long responseTimeSum = 0; // the sum of all responses in conversation
+    long responseCount = 0; // the count of all responses in conversation
+    long userResponse = 0; // take the time of the user response
+    long averageResponse = 0; // the average response time
+
+
+    boolean isTeamMember = true; // initialize isTeamMember to true because we have to calculate the response time starting from the user
+
+
+    for (Message message : messages ) {
+      /*
+      if it's not team member and the last response was from a team member
+      record when the user sent the message
+
+       */
+      if(!message.isTeamMember()  && isTeamMember ) {
+        userResponse = message.getCreatedAt();
+        isTeamMember = false;
+
+       }
+      /*
+      if it's a team member and it's team member turn to response
+      increment the counter by 1
+      add the current response time to the response time sum
+
+
+       */
+
+      else if(message.isTeamMember()  && !isTeamMember ){
+        responseCount = responseCount + 1;
+        responseTimeSum = responseTimeSum + (message.getCreatedAt() - userResponse );
+        isTeamMember=true;
+
+
+      }
+    }
+
+    if(responseCount > 0) {
+      averageResponse = responseTimeSum / responseCount;
+    }
+
+
     // implement me!
 
     return ConversationResponseMetric.builder()
         .setConversationId(conversation.getId())
-        .setAverageResponseMs(0)
+        .setAverageResponseMs(averageResponse)
         .build();
   }
 }
